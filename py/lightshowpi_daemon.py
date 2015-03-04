d88e67f6f158239e1023823874d4d5b8c7b51768
Chad Brochard <chad.brochard@gmail.com>
2015-03-04 01:59:29 +0000

    make port 80 default
    modifed the lightshowpi daemon start up script to make port 80 the default.
    An optional port number can be provided to use a port other than 80.

--- py/lightshowpi_daemon.py
+++ py/lightshowpi_daemon.py
@@ -15,16 +15,18 @@ if __name__ == "__main__":
 	daemon = MyDaemon(env+'/py/daemon-lightshowpi.pid')
 	if (len(sys.argv) == 3 or len(sys.argv) == 2) :
 		if  'start' == sys.argv[1]:
-                        del sys.argv[1:2]
-			daemon.start()
-		elif 'stop' == sys.argv[1]:
-			daemon.stop()
-		elif 'restart' == sys.argv[1]:
-			daemon.restart()
-		else:
-			print "Unknown command"
-			sys.exit(2)
-		sys.exit(0)
+                    if len(sys.argv) ==2:
+                      sys.argv.append('80')
+                    del sys.argv[1:2]
+	            daemon.start()
+	        elif 'stop' == sys.argv[1]:
+		    daemon.stop()
+	        elif 'restart' == sys.argv[1]:
+		    daemon.restart()
+	        else:
+		    print "Unknown command"
+		    sys.exit(2)
+		    sys.exit(0)
 	else:
-		print "usage: %s start|stop|restart [Optional] port" % sys.argv[0]
+		print "usage: %s start|stop|restart [Optional] port (default port is 80)" % sys.argv[0]
 		sys.exit(2)
