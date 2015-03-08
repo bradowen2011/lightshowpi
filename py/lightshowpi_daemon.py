4e604840dc2626f2ec4b13de94fd68be857aec9a
Chad Brochard <chad.brochard@gmail.com>
2015-03-08 03:08:11 +0000

    Include harward intilation in daemon Made some corrections to the lightshowpi daemon to intilize the hardware configuration.

--- py/lightshowpi_daemon.py
+++ py/lightshowpi_daemon.py
@@ -2,21 +2,24 @@
 
 import sys, time
 from daemon import Daemon
-from webapp import app,env
+from webapp import app,env,hc,web
 
 
 
 class MyDaemon(Daemon):
 	def run(self):
 		while True: 
+			hc.initialize()
+    			#web.config.debug = false  #Added to stop multiple instance of classes being created
 			app.run()
+    			hc.clean_up()
 
 if __name__ == "__main__":
-	daemon = MyDaemon(env+'/py/daemon-lightshowpi.pid')
+	daemon = MyDaemon(env+'/daemon-lightshowpi.pid')
 	if (len(sys.argv) == 3 or len(sys.argv) == 2) :
-		if  'start' == sys.argv[1]:
-                    if len(sys.argv) ==2:
+		if len(sys.argv) ==2:
                       sys.argv.append('80')
+		if  'start' == sys.argv[1]:                    
                     del sys.argv[1:2]
 	            daemon.start()
 	        elif 'stop' == sys.argv[1]:
